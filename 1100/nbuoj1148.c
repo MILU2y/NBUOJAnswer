d3e260f69ab92a2b111ac4553bf4abd200c86545
MILU2y <151505870+MILU2y@users.noreply.github.com>
2023-12-04 20:10:02 +0800

    Update nbuoj1148.c

---
+++ 1100/nbuoj1148.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#define N 1000
+int main() {
+    char str[N];
+    gets(str);
+    int i;
+    int num[10] = {0};
+    for (i = 0; str[i] != '\0'; i++) {
+        if ('0' <= str[i] && str[i] <= '9') {
+            int digit = str[i] - '0'; 
+            num[digit] += 1;
+        }
+    }
+    for (i = 0; i < 10; i++) {
+        if (num[i] != 0) {
+            printf("%d:%d\n", i, num[i]);
+        }
+    }
+    return 0;
+}
