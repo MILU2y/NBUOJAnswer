b0475820cfa9a118f3425156616c73aef333c3ab
MILU2y <151505870+MILU2y@users.noreply.github.com>
2023-12-04 20:04:08 +0800

    Update nbuoj1172.c

---
+++ 1100/nbuoj1172.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#define N 1000
+int main()
+{
+	int ten, eight[1000];
+    int i,j;
+	scanf("%d",&ten);
+	for(i=0;ten!=0;i++)
+    {
+		eight[i]=ten%8;
+		ten=ten/8;
+	}
+    for(j=i-1;j>=0;j--)
+	printf("%d ",eight[j]);
+	printf("\n");
+	return 0;
