f4cf763d6ac1f132d8193278b17f8d2f8b61dc23
MILU2y <151505870+MILU2y@users.noreply.github.com>
2023-12-04 20:06:35 +0800

    Update nbuoj1298.c

---
+++ 1200/nbuoj1298.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int a[5][5];
+	int i , j;
+	for(i=0;i<5;i++)
+	for(j=0;j<5;j++)
+	scanf("%d",&a[i][j]);
+	for(i=0;i<5;i++)
+    for(j=i+1;j<5;j++)
+	a[i][j]=0;
+	for(i=0;i<5;i++)
+	{for(j=0;j<5;j++)
+	printf("%4d",a[i][j]);
+	printf("\n");}
+	return 0;
