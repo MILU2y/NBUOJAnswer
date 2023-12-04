b86ce306d9ab296afc04a7980b47d93c5c1470c3
MILU2y <151505870+MILU2y@users.noreply.github.com>
2023-12-04 20:08:03 +0800

    Update nbuoj1139.c

---
+++ 1100/nbuoj1139.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	char str[9];
+	gets(str);
+	int i;
+	for(i=0;i<9;i++)
+    {
+		if('a'<=str[i]&&str[i]<='v'||'A'<=str[i]&&str[i]<='V')
+		str[i]+=4;
+		if('w'<str[i]&&str[i]<='z'||'W'<=str[i]&&str[i]<='Z')
+        str[i]=str[i]+4-26;
+	}
+	for(i=0;i<9;i++)
+	printf("%c",str[i]);
+	printf("\n");
+	return 0;
+}
